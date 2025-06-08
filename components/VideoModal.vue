490f4ff5332acef3f077c7ce8fad377a540d6654
Artem <myartoym@gmail.com>
2025-06-08 11:34:07 +0300

    fix

--- components/VideoModal.vue
+++ components/VideoModal.vue
@@ -103,7 +103,7 @@ h2 {
 .modal-videos {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(2, 1fr);
+  // grid-template-rows: repeat(2, 1fr);
   gap: 10px;
   @media (max-width: 400px) {
     grid-template-columns: repeat(2, 1fr);
